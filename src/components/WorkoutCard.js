ce9b90217a5d01c898854bd2c9ec9a178bf7faa0
JPDevAccX <perciv.jam@gmail.com>
2023-05-23 05:00:30 +0100

    WorkoutCard HTML fix

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -7,16 +7,15 @@ const WorkoutCard = (props) => {
       <Card.Img variant="top" src={props.imgUrl} alt="Logo" />
       <Card.Body>
         <Card.Title>
-            {props.title}
+        	{props.title}
         </Card.Title>
-        <Card.Text>
-        Target <h4>{props.target}</h4>
-        Body Part <h4>{props.part}</h4>
-        Equipment <h4>{props.equiment}</h4>
+        <Card.Text as="div">
+					Target <h4>{props.target}</h4>
+					Body Part <h4>{props.part}</h4>
+					Equipment <h4>{props.equiment}</h4>
         </Card.Text>
       </Card.Body>
     </Card>
-    
   );
 };
 
